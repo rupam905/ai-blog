05aa9e6a2a27f06079b4dfb9b85c0e90f0bc0076
rupam905 <rupamdutta905.com>
2026-01-25 10:14:03 +0530

    server configs

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
-});
+// app.listen(PORT, () => {
+//   console.log("Server is running on port " + PORT);
+// });
 
 export default app;
